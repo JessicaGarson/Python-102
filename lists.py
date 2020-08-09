29bd81effc6d990b0620cb9c1708ed3eacee3f2f
Jessica Garson <jgarson@twitter.com>
2020-08-09 16:12:40 -0400

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,10 @@
+days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
+'Saturday', 'Sunday']
+
+for day in days_of_the_week:
+    print(day)
+
+python_102=['Jasmin','Sanaz','Sofia']
+
+for attendee in python_102:
+    print('{} is currently watching a Python class'.format(attendee))
