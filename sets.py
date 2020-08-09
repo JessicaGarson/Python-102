b4d98c85f31eb6d3b986694280e38a5313ef2d3f
Jessica Garson <jgarson@twitter.com>
2020-08-09 16:11:52 -0400

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,8 @@
+colors = {'blue', 'green', 'purple', 'orange'}
+print(colors)
+
+a_set = {5, 6, 9, 2, 1, 4}
+b_set = {9, 4, 6, 2, 14, 5, 6}
+print(a_set.union(b_set))
+print(a_set.intersection(b_set))
+print(a_set.difference(b_set))
