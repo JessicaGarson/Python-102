e921260173dcd286bc8aa909d6ab82957240ea18
Jessica Garson <jgarson@twitter.com>
2020-08-09 16:07:58 -0400

    Create cond.py

---
+++ cond.py
@@ -0,0 +1,8 @@
+weather = 'sunshine days'
+
+if weather == 'raining':
+    print("bring an umbrella")
+elif weather == 'muuuggyy':
+    print("oh, no. I hope it gets better")
+else:
+    print("I hope it's sunny today")
