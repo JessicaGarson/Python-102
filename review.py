d268da631eefd7d45b175d13c15c914482ba2e7b
Jessica Garson <jgarson@twitter.com>
2020-08-09 16:16:10 -0400

    Create review.py

---
+++ review.py
@@ -0,0 +1,22 @@
+name = 'generation data'
+age = 2
+city = 'DC'
+print(name)
+print(age)
+print(city)
+
+integer = 2
+print(integer)
+floating_point = 3.5
+print(floating_point)
+
+double = "hello"
+print(double)
+single = "hi"
+print(single)
+
+how_are_you = input("How are you?\n")
+print(how_are_you)
+
+interaction = "Hi {}, you are {} years old and you live in {}".format(name, age, city)
+print(interaction)
